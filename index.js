9cd3cd7441edff72da1cf0fbbdc5d9d745b37cd9
elaheerazob <ridwanelahee282@gmail.com>
2022-08-14 10:43:16 +0600

    add new 1

--- index.js
+++ index.js
@@ -1,87 +1,107 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-const app =express()
+const app = express();
 
 //middle ware
 app.use(cors());
 app.use(express.json());
 
-
 //wtLxw8UCavgOodgR
 
-
-
-
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
-const uri = "mongodb+srv://ridwan:wtLxw8UCavgOodgR@cluster0.tdnhswv.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+  "mongodb+srv://ridwan:wtLxw8UCavgOodgR@cluster0.tdnhswv.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try{
-        const notesCollection = client.db("notesTaker").collection("notes");
+  try {
+    const notesCollection = client.db("notesTaker").collection("notes");
 
-        //get api to read all nodes
-        //http://localhost:5000/notes
-        app.get('/notes',async(req,res)=>{
-            // const q =req.query;
-            const cursor = notesCollection.find({});
-            const result =await cursor.toArray();
-            res.send(result)
-        })
+    //get api to read all nodes
+    //http://localhost:5000/notes
+    app.get("/notes", async (req, res) => {
+      // const q =req.query;
+      const cursor = notesCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-        //create nodesTaker
-        //http://localhost:5000/note
-       /*  {
+    //search
+
+    app.get("/notes/:name", async (req, res) => {
+      const query = req.params.name;
+      const singleData = await notesCollection.find({ name: query }).toArray();
+      res.send(singleData);
+    });
+
+    //querry pramiter
+
+    /*  app.get("/notes", async (req, res) => {
+      const q = req.query;
+      console.log(q);
+
+      const cursor = notesCollection.find( q);
+
+      const result = await cursor.toArray();
+
+      res.send(result);
+    }); */
+
+    //create nodesTaker
+    //http://localhost:5000/note
+    /*  {
             "name": "Elahee",
             "age": 25,
             "address": "Rangpur Bangladesh"
         } */
 
-        app.post('/note',async (req,res)=>{
-            const data = req.body;
+    app.post("/note", async (req, res) => {
+      const data = req.body;
+      const result = await notesCollection.insertOne(data);
 
-            const result =await notesCollection.insertOne(data);
+      res.send(result);
+    });
 
-            console.log(data);
-            res.send(result)
-        })
+    // update nodesTaker
+    //http://localhost:5000/note/62f667935ec60a9c7c0699b1
+    app.put("/note/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      console.log(id);
 
-        // update nodesTaker
-        //http://localhost:5000/note/62f667935ec60a9c7c0699b1
-        app.put('/note/:id',async (req,res)=>{
-            const id =req.params.id;
-            const data = req.body;
-            console.log(id);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: data.name,
+          age: data.age,
+          address: data.address,
+        },
+      };
+      const result = await notesCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
 
-            const filter = {_id: ObjectId(id)};
-            const options = { upsert: true };
-            const updateDoc = {
-                $set: {
-                    name: data.name,
-                    age: data.age,
-                    address: data.address,
-                },
-              };
-              const result = await notesCollection.updateOne(filter, updateDoc, options);
-              res.send(result)
-        })
+    //delete NodesTaker
+    app.delete("/note/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await notesCollection.deleteOne(filter);
+      res.send(result);
+    });
 
-        //delete NodesTaker
-        app.delete('/note/:id',async (req,res) =>{
-            const id =req.params.id;
-            const filter ={_id: ObjectId(id)};
-            const result = await notesCollection.deleteOne(filter);
-            res.send(result);
-        })
-
-
-        console.log('Connected DB');
-    }
-    finally{
-
-    }
+    console.log("Connected DB");
+  } finally {
+  }
 }
 run().catch(console.dir);
 
@@ -92,11 +112,10 @@ run().catch(console.dir);
 // console.log('connected DB');
 // });
 
+app.get("/", (req, res) => {
+  res.send("Hello razob");
+});
 
-app.get('/',(req,res) =>{
-    res.send('Hello razob')
-})
-
-app.listen(port,() =>{
-    console.log(`Connect`,port)
-})
+app.listen(port, () => {
+  console.log(`Connect`, port);
+});
